6dc8e015bf141864bb4c9fbb437c93a18a99187b
Édouard Lopez <contact@edouard-lopez.com>
2022-12-02 21:23:03 +0100

    test(distroless): test scenarios for ubuntu/debian/alpine

--- src/distroless/install.sh
+++ src/distroless/install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
 
-source "$(dirname "$0")"/git/install.sh; install
 source "$(dirname "$0")"/pacapt/install.sh; install
+source "$(dirname "$0")"/git/install.sh; install
