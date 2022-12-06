4e654905577f9f0b2ac30428426eb1291c712c81
Édouard Lopez <contact@edouard-lopez.com>
2022-12-06 13:55:17 +0100

    refactor(features): use hardlink to ensure.sh

--- src/bats/ensure.sh
+++ src/bats/ensure.sh
@@ -1,1 +1,28 @@
-../distroless/ensure.sh
+#!/usr/bin/env sh
+
+ensure() {
+    packages="$*"
+    echo "Ensure availability of: $packages "
+    ROOT_USER_ID=0
+    [ "$(id -u)" -ne $ROOT_USER_ID ] && privileges="sudo "
+
+    set -x
+    type apk >/dev/null 2>&1 && echo 'Alpine' \
+        && "$privileges"apk add  \
+                    --no-cache \
+                $packages 
+    type apt >/dev/null 2>&1 && echo 'Debian/Ubuntu' \
+        && "$privileges"apt-get update \
+        && "$privileges"apt-get install \
+                                --yes \
+                                --no-install-recommends \
+                        $packages  \
+        && "$privileges"rm -rf /var/lib/apt/lists/*;
+    type dnf >/dev/null 2>&1 && echo 'RockyLinux/Centos/Fedora' \
+        && dnf update \
+        && dnf install \
+                    -y \
+                $packages 
+    
+    echo "Ensure… done"
+}
--- src/ensure.sh
+++ src/ensure.sh
@@ -5,20 +5,24 @@ ensure() {
     echo "Ensure availability of: $packages "
     ROOT_USER_ID=0
     [ "$(id -u)" -ne $ROOT_USER_ID ] && privileges="sudo "
+
+    set -x
     type apk >/dev/null 2>&1 && echo 'Alpine' \
         && "$privileges"apk add  \
                     --no-cache \
-                "$packages"
+                $packages 
     type apt >/dev/null 2>&1 && echo 'Debian/Ubuntu' \
         && "$privileges"apt-get update \
         && "$privileges"apt-get install \
                                 --yes \
                                 --no-install-recommends \
-                        "$packages" \
+                        $packages  \
         && "$privileges"rm -rf /var/lib/apt/lists/*;
-    type dnf >/dev/null 2>&1 && echo 'Centos/Fedora' \
+    type dnf >/dev/null 2>&1 && echo 'RockyLinux/Centos/Fedora' \
         && dnf update \
         && dnf install \
-                    --yes \
-                "$packages"
+                    -y \
+                $packages 
+    
+    echo "Ensure… done"
 }
